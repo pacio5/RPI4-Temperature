63546cc85ccb13ac6b7d7634478ac5841a43f3cf
Elia Pacioni <eliapacioni@gmail.com>
2023-03-09 13:21:21 +0100

    Remove unsed variable

--- LM75.py
+++ LM75.py
@@ -1,9 +1,5 @@
 from smbus2 import SMBus
 
-LM75_ADDRESS		 = 0x48
-
-LM75_TEMP_REGISTER 	 = 0x00
-
 class LM75(object):
 	def __init__(self, busnum=1):
 		self._address = 0x48
@@ -14,5 +10,4 @@ class LM75(object):
 		raw = self._bus.read_word_data(self._address, self._temp_register) & 0xFFFF
 		raw = ((raw << 8) & 0xFF00) + (raw >> 8)
 		return raw /256
-
-        
+	
